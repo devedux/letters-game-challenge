609fa66955e55156d35975db2bef4d1da4b81c86
devedux <devedux@gmail.com>
2021-12-14 16:13:46 -0500

    add styles for each tile

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -28,14 +28,38 @@ export default function Board({tiles, onClick, board, word}: BoardProps) {
 
 const BoardStyled = styled.div<{ board: number }>`
   display: grid;
-  grid-template-columns: ${props => `repeat(${props.board}, 50px)`};
-  grid-auto-rows: 50px;
-  grid-gap: 8px;
+  grid-template-columns: ${props => `repeat(${props.board}, 80px)`};
+  grid-auto-rows: 80px;
+  grid-gap: 15px;
 `
 const TileStyled = styled.div<{ isSelected: boolean }>`
+  position: relative;
+  border: 2px solid transparent;
+  border-radius: 8px;
+  background: linear-gradient(#FAD15C, #F77120);
+  background-clip: padding-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+  font-size: 30px;
+  text-shadow: -2px -1px 3px rgba(0, 0, 0, 0.3);
   cursor: pointer;
-  border: 1px solid;
+  &::after {
+    position: absolute;
+    top: -2px;
+    bottom: -2px;
+    left: -2px;
+    right: -2px;
+    background: linear-gradient(#F4505F, #A2071E);
+    content: '';
+    z-index: -1;
+    border-radius: 8px;
+  }
+
   ${props => props.isSelected && `
-    background: red;  
+    background: linear-gradient(#F4505F, #A2071E);
+    background-clip: padding-box;
   `}
 `
