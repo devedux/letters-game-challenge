2173a365ca5562efd6deb817f6fc2d67de8dff52
devedux <devedux@gmail.com>
2021-12-14 12:26:40 -0500

    Valid remaining tiles and previous tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import * as React from 'react'
-import {getTiles} from './utils/letters-game'
+import {getTiles, validTiles} from './utils/letters-game'
 import Board from './components/board'
 
-function validTiles(lastSelected: number) {
-  const leftTiles = [0, 4, 8, 12]
-  const rightTiles = [3, 7, 11, 15]
 
-  let nextTile = rightTiles.includes(lastSelected) ? lastSelected - 1 : lastSelected + 1
-  let topTile = lastSelected - 4
-  let topNextTile = nextTile - 4
-  let bottomTile = lastSelected + 4
-  let bottomNextTile = nextTile + 4
-
-  let prevTile;
-  let topPrevTile;
-  let bottomPrevTile;
-  if (!leftTiles.includes(lastSelected) && !rightTiles.includes(lastSelected)) {
-    prevTile = lastSelected - 1
-    topPrevTile = prevTile - 4
-    bottomPrevTile = prevTile + 4
-  }
-
-  return [
-    topPrevTile,
-    topTile,
-    topNextTile,
-    prevTile,
-    nextTile,
-    bottomPrevTile,
-    bottomTile,
-    bottomNextTile
-  ].filter(tile => tile && tile >= 0 && tile <= 15)
-}
 
 export default function App() {
   const [tiles, setTiles] = React.useState<Array<string>>(
@@ -41,7 +12,6 @@ export default function App() {
   const [word, setWord] = React.useState('')
 
   function selectTile(letter: string, position: number) {
-    console.log(letter,'-', position)
     if (lastSelected !== undefined) {
       if (word.includes(letter)) {
         alert('esta letra ya fue seleccionada')
@@ -60,6 +30,7 @@ export default function App() {
     <div>
       Letters Game!
       <Board tiles={tiles} onClick={selectTile} />
+      {word}
     </div>
   );
 }
--- src/utils/letters-game.ts
+++ src/utils/letters-game.ts
@@ -1,12 +1,56 @@
+import {add, subs} from './helper'
 import dataBoard from '../data/test-board-1.json'
 // import board2 from '../data/test-board-2.json'
+const sumBy4 = add(4)
+const sumBy1 = add(1)
+const subsBy4 = subs(4)
+const subsBy1 = subs(1)
 
 function getTiles(): Array<string> {
   return shuffle(dataBoard.board)
+}
+
+function rangeOfTiles(tile: number | undefined) {
+  return tile !== undefined && tile >= 0 && tile <= 15
+}
+
+type ArrayNumber = Array<number> 
+type ReturnPrevTiles = Array<undefined> | ArrayNumber
+
+function getPreviousTiles(leftTiles: number[], rightTiles: number[], lastSelected: number): ReturnPrevTiles {
+  if (leftTiles.includes(lastSelected) && rightTiles.includes(lastSelected)) {
+    return []
+  }
+
+  const prevTile = subsBy1(lastSelected)
+  const topPrevTile = subsBy4(prevTile)
+  const bottomPrevTile = sumBy4(prevTile)
+  return [prevTile, topPrevTile, bottomPrevTile]
+}
+
+function getRemainingTiles(
+  rightTiles: number[],
+  lastSelected: number
+): ArrayNumber {
+  const nextTile = rightTiles.includes(lastSelected) ? subsBy1(lastSelected) : sumBy1(lastSelected)
+  const topTile = subsBy4(lastSelected)
+  const topNextTile = subsBy4(nextTile)
+  const bottomTile = sumBy4(lastSelected)
+  const bottomNextTile = sumBy4(nextTile)
+  return [topTile, topNextTile, nextTile, bottomTile, bottomNextTile]
+}
+
+function validTiles(lastSelected: number) {
+  const leftTiles = [0, 4, 8, 12]
+  const rightTiles = [3, 7, 11, 15]
+
+  const previousTiles = getPreviousTiles(leftTiles, rightTiles, lastSelected)
+  const remainingTiles = getRemainingTiles(rightTiles, lastSelected)
+  return [...remainingTiles,...previousTiles].filter(rangeOfTiles)
 }
 
 function shuffle(list: Array<any>) {
   return list.sort(() => Math.random() - 0.5)
 }
 
-export {getTiles, shuffle}
+export {getTiles, shuffle, validTiles}
